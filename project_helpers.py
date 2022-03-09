3eb1030eaca51ee2d6a3046bd6f6a77f7f686198
jose2mil18 <40545963+jose2mil18@users.noreply.github.com>
2022-03-08 20:58:00 -0400

    version 1

---
+++ project_helpers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 19:43:12 2019
+
+@author: JuanGabriel
+"""
+
+
+from tensorflow.keras.datasets import fashion_mnist
+from scipy.misc import imsave
+
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+for i in range(5):
+    imsave(name="uploads/{}.png".format(i), arr=X_test[i])
